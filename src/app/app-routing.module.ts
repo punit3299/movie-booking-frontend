683de3c9b2fd86c8442a2fa4e29d4ac17c2e3542
Punit Raj <raj.punit3299@gmail.com>
2020-08-15 21:26:31 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerHomeComponent } from './customer/customer-home/customer-home.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'admin', component: AdminComponent,
+    children: [
+      { path: 'admin-home', component: AdminHomeComponent },
+      { path: '**', component: AdminHomeComponent },
+    ]
+  },
+  {
+    path: 'customer', component: CustomerComponent,
+    children: [
+      { path: 'customer-home', component: CustomerHomeComponent },
+      { path: '**', component: CustomerHomeComponent }
+    ]
+  },
+  { path: '**', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TopNavbarComponent } from './shared/navbars/top-navbar/top-navbar.component';
+import { BottomNavbarComponent } from './shared/navbars/bottom-navbar/bottom-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { AdminComponent } from './admin/admin.component';
+import { CustomerHomeComponent } from './customer/customer-home/customer-home.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    TopNavbarComponent,
+    BottomNavbarComponent,
+    CustomerComponent,
+    AdminComponent,
+    CustomerHomeComponent,
+    AdminHomeComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
