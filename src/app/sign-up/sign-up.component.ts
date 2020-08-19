1a87f4cbe34d7796eb0fd8f108f3e76f435e3fd7
forgivingyou <000raghuvanhsi@gmail..com>
2020-08-19 21:26:00 +0530

    updated

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -54,7 +54,7 @@ export class SignUpComponent implements OnInit {
           }
           else {
             this.customerService.addCustomer(this.registerForm.value).subscribe(data => {
-              this.customer=data;
+              this.customerService.customer=data;
               localStorage.setItem("CustomerId", this.registerForm.controls.email.value);
               this.router.navigateByUrl('/customer');
               this.toastrService.success("Welcom " + this.customerService.customer.customerName);
