819422bc00df48a4942295b9a2a8198ada8bd6c6
forgivingyou <000raghuvanhsi@gmail..com>
2020-08-20 09:16:06 +0530

    updated

--- src/app/customer/view-bookings/view-bookings.component.ts
+++ src/app/customer/view-bookings/view-bookings.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from 'src/app/shared/services/customer/customer.service';
 import { Bookings } from 'src/app/shared/models/bookings.model';
-<<<<<<< HEAD
 import { ToastrService } from 'ngx-toastr';
-import { Ticket } from 'src/app/shared/models/ticket.model';
 import { Booking } from 'src/app/shared/models/booking.model';
-=======
->>>>>>> 4b03b4bcab83643bffd4100bcc71a6f9c998c910
 
 @Component({
   selector: 'app-view-bookings',
@@ -14,7 +10,6 @@ import { Booking } from 'src/app/shared/models/booking.model';
   styleUrls: ['./view-bookings.component.css']
 })
 export class ViewBookingsComponent implements OnInit {
-<<<<<<< HEAD
   public bookings:Bookings[];
 
 customerId:number=1160;
@@ -22,34 +17,17 @@ customerId:number=1160;
     this.customerService.getPreviousBookings(this.customerId).subscribe(data=>{this.bookings=data, console.log(this.bookings)},error=>{console.log(error)});
    }
  t:any;
-=======
-  public bookings:Bookings[]=[{
-    bookingId:1121,
-    bookingDate:null,
-    totalCost:0,
-    movie:null,
-      status:null,
-      transaction:null,
-      ticket:null,
-      show:null}]
 
-customerId:number=1160;
-  constructor(private customerService:CustomerService) {
-    this.customerService.getPreviousBookings(this.customerId).subscribe(data=>{this.bookings=data, console.log(this.bookings)},error=>{console.log(error)});
-   }
- 
->>>>>>> 4b03b4bcab83643bffd4100bcc71a6f9c998c910
 
   ngOnInit() {
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     if (filterValue != '') {
-<<<<<<< HEAD
       this.bookings = this.bookings.filter(booking => booking.movie.toLowerCase().startsWith(filterValue.toLowerCase()) || booking.show.theatreName.startsWith(filterValue.toLowerCase()));
-=======
-      this.bookings = this.bookings.filter(booking => booking.movie.toLowerCase().startsWith(filterValue.toLowerCase()) || booking.show.theatre.theatreName.startsWith(filterValue.toLowerCase()));
->>>>>>> 4b03b4bcab83643bffd4100bcc71a6f9c998c910
+
+      this.bookings = this.bookings.filter(booking => booking.movie.toLowerCase().startsWith(filterValue.toLowerCase()) || booking.show.theatreName.startsWith(filterValue.toLowerCase()));
+
     }
     else {
       
@@ -57,15 +35,8 @@ customerId:number=1160;
   }
 
 }
-<<<<<<< HEAD
 booking:Booking;
 cancelTicket(booking:Booking){
-  this.customerService.getBooking(booking.bookingId).subscribe(data=>{
-    console.log("booking");
-    console.log(data);
-    this.booking=data;
-  })
-
   this.customerService.cancelTicket(this.customerId,booking.ticket).subscribe(data=>{
     console.log("ticket");
     console.log(data);
@@ -74,6 +45,4 @@ cancelTicket(booking:Booking){
   })
 }
 
-=======
->>>>>>> 4b03b4bcab83643bffd4100bcc71a6f9c998c910
 }
--- src/app/shared/services/customer/customer.service.ts
+++ src/app/shared/services/customer/customer.service.ts
@@ -10,6 +10,8 @@ import { Shows } from '../../models/shows.model';
 import { BookedDetailsOfTicket } from '../../models/booked-details-of-ticket.model';
 
 import { Booking } from '../../models/booking.model';
+import { Credentials } from '../../models/credentials.model';
+import { BookTicketDetails } from '../../models/book-ticket-details.model';
 
 
 @Injectable({
@@ -42,6 +44,36 @@ export class CustomerService {
 
   addMoneyToWallet(amount:number,customer:Customer){
     return this.http.put<Customer>(this.url+"/addMoney/"+amount,customer);
+  }
+
+  bookedSeatArray(showId: number) {
+    let url = "http://localhost:8083/customer/show/seats/" + showId;
+    return this.http.get<number[]>(url);
+
+  }
+
+  bookTicket(ticketDetails: BookTicketDetails): Observable<any> {
+    console.log("in angular service");
+    console.log(ticketDetails);
+    let url = "http://localhost:8083/customer/bookSeat/new";
+    return this.http.post(url, ticketDetails);
+  }
+
+  showTicket(ticketDetail: BookedDetailsOfTicket) {
+
+    this.ticketDetail = ticketDetail;
+  }
+
+  validateEmail(email:string){
+    return this.http.get<boolean>(this.url+"/validateEmail/"+email);
+  }
+
+  validateContactNumber(number:number){
+    return this.http.get<boolean>(this.url+"/validateContactNumber/"+number);
+  }
+
+  validateCredential(credentials:Credentials){
+    return this.http.post(this.url+"/validateCredential",credentials,{ responseType: 'text' as 'json' });
   }
 
 
